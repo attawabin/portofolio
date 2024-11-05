852249ab1c87a2d6997b092cac691af13ea04f0a
attawabin <attawabinsofy68@gmail.com>
2024-11-05 09:52:40 +0700

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <div className="bg-[url('https://images.unsplash.com/photo-1598606105252-3e70708b8a7b?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] h-screen w-full bg-cover">
+          <p>Hello World</p>
+          <p>Saya Gebin dari Web Developer</p>
+          <p>
+            Bridging design and technology to create intuitive and engaging user
+            experiences
+          </p>
+          <button className="bg-red-400">Check My Github</button>
+        </div>
+      </div>
+      <div>
+        <p>My Project</p>
+        <p>Some project i have worked on</p>
+      </div>
+      <div>
+        <p>Tech Stack</p>
+        <p>Tools and Frameworks I've used</p>
+      </div>
+    </div>
+  );
+}
