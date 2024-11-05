b33219df48c43256c9eccd3e1d801fac9a692b30
attawabin <attawabinsofy68@gmail.com>
2024-11-05 10:01:54 +0700

    update

--- src/app/portofolio/page.tsx
+++ src/app/portofolio/page.tsx
@@ -1,1 +1,13 @@
- 
+import * as React from "react";
+
+interface IPortfolioProps {}
+
+const Portfolio: React.FunctionComponent<IPortfolioProps> = (props) => {
+  return (
+    <div>
+      <p>Portfolio</p>
+    </div>
+  );
+};
+
+export default Portfolio;
